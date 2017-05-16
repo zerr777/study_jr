147b518c1dce1279c557225ca3cd60310eec69ae
zerr777 <avo_86@mail.ru>
2017-05-17 00:15:54 +0300

    16.05

--- 1.JavaSyntax/src/com/javarush/task/task07/task0708/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0708/Solution.java
@@ -10,6 +10,20 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        int max = 0;
+        int ind = 0;
+        for (int i = 0; i < 5; i++) {
+            list.add(reader.readLine());
+            if (list.get(i).length() > max) {
+                max = list.get(i).length();
+                ind = i;
+            }
+        }
+        for (String ls : list)
+            if (ls.length() == list.get(ind).length())
+                System.out.println(ls);
         //напишите тут ваш код
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task07.task0720;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /* 
 Перестановочка подоспела
@@ -11,7 +12,21 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+        ArrayList<String> list = new ArrayList<>();
 
+        for (int i = 0; i < n; i++) {
+            list.add(reader.readLine());
+        }
+
+        for (int i = 0; i < m; i++) {
+            list.add(list.size(), list.get(0));
+            list.remove(0);
+        }
+
+        for (String ls : list)
+            System.out.println(ls);
         //напишите тут ваш код
     }
 }
