c6e085d3ede96b77fe2f8508118cb189b95c6a00
zerr777 <avo_86@mail.ru>
2017-05-28 21:54:46 +0300

    28.05

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1308/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task13.task1308;
+
+/* 
+Жив или нет
+*/
+
+public class Solution {
+    public interface Person {
+        public boolean isAlive();
+    }
+    public interface Presentable extends Person {
+
+    }
+    public static void main(String[] args) throws Exception {
+    }
+}
