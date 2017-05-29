ea1eaea36c4384b1a90bdd775ebbb77a2adae96b
zerr777 <avo_86@mail.ru>
2017-05-29 15:13:43 +0300

    29.05

--- 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1309/Solution.java
@@ -5,6 +5,12 @@ package com.javarush.task.task13.task1309;
 */
 
 public class Solution {
+    public interface Movable {
+        public Double speed();
+    }
+    public interface Flyable extends Movable {
+        public Double speed(Flyable flyable);
+    }
     public static void main(String[] args) throws Exception {
     }
 }
