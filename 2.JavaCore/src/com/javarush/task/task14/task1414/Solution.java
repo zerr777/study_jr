fa9f49c02c0dac1921aed975399362226d214457
zerr777 <avo_86@mail.ru>
2017-06-07 18:48:36 +0300

    07.06

--- 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -4,8 +4,29 @@ package com.javarush.task.task14.task1414;
 MovieFactory
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Movie movie = null;
+        while (true){
+            String s = reader.readLine();
+            if (s.equals("soapOpera")){
+                movie = MovieFactory.getMovie(s);
+                System.out.println(movie.getClass().getSimpleName());}
+            else if (s.equals("cartoon")){
+                movie = MovieFactory.getMovie(s);
+                System.out.println(movie.getClass().getSimpleName());}
+            else if (s.equals("thriller")){
+                movie = MovieFactory.getMovie(s);
+                System.out.println(movie.getClass().getSimpleName()); }
+            else {
+                movie = MovieFactory.getMovie(s);
+                break;
+            }
+        }
         //ввести с консоли несколько ключей (строк), пункт 7
 
         /*
@@ -25,6 +46,10 @@ public class Solution {
             if ("soapOpera".equals(key)) {
                 movie = new SoapOpera();
             }
+            else if ("cartoon".equals(key))
+                movie = new Cartoon();
+            else if ("thriller".equals(key))
+                movie = new Thriller();
 
             //напишите тут ваш код, пункты 5,6
 
@@ -37,14 +62,12 @@ public class Solution {
 
     static class SoapOpera extends Movie {
     }
-
-    public static class Cartoon {
-
-    }
-
-    public static class Thriller {
+    static class Cartoon  extends Movie{
 
     }
 
+    static class Thriller extends Movie{
+
+    }
     //Напишите тут ваши классы, пункт 3
 }
