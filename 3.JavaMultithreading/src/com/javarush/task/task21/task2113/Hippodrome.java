453087971bf25ddd1e75ff23f53c701afb3c9028
zerr777 <avo_86@mail.ru>
2017-06-10 13:26:09 +0300

    10.06

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -9,20 +9,42 @@ import java.util.List;
  */
 public class Hippodrome {
 
+    public Horse getWinner() {
+        int win = 0;
+        for (int i = 1; i < horses.size(); i++) {
+            win = horses.get(i).distance > horses.get(win).distance ? i : win;
+        }
+        return horses.get(win);
+    }
+
+    public void printWinner(){
+        System.out.println("Winner is " + getWinner().getName()+ "!");
+    }
+
     public void run(){
         for (int i = 1; i <= 100; i++) {
             move();
             print();
             try {
-                Thread.sleep(200);
+                Thread.sleep(20);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
         }
     }
-    public void move(){}
-    public void print(){}
+    public void move(){
+        for (Horse horse : horses)
+            horse.move();
+    }
+    public void print(){
+        for (Horse horse : horses)
+            horse.print();
+        for (int i = 0; i < 3; i++) {
+            System.out.println();
+
+        }
+    }
 
     static Hippodrome game;
 
@@ -42,6 +64,8 @@ public class Hippodrome {
         Horse horse3 = new Horse("Horse3", 3, 0);
 
         game = new Hippodrome(Arrays.asList(horse1,horse2, horse3));
+        game.run();
+        game.printWinner();
 
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -7,6 +7,16 @@ public class Horse {
     String name;
     double speed;
 
+    public void move(){
+        distance += speed * Math.random();
+    }
+    public void print(){
+        for (int i = 0; i < (int)distance; i++) {
+            System.out.print(".");
+        }
+        System.out.println(name);
+    }
+
     public void setName(String name) {
         this.name = name;
     }
