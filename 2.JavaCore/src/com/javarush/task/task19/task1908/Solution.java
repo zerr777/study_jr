8ded45d33fe5236b04fd3168495641d725bb8a10
zerr777 <avo_86@mail.ru>
2017-07-07 15:00:55 +0300

    01.07

--- 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -4,7 +4,29 @@ package com.javarush.task.task19.task1908;
 Выделяем числа
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader fr = new BufferedReader(new FileReader(reader.readLine()));
+        BufferedWriter fw = new BufferedWriter(new FileWriter(reader.readLine()));
+        String s = "";
+        while (fr.ready()){
+            s = s + (char)fr.read();
+        }
+        String[] arr = s.split( " ");
+        for(String t: arr) {
+            try {
+                int temp = Integer.parseInt(t);
+                fw.write(temp + " ");
+            }
+            catch (NumberFormatException e){
+
+            }
+        }
+        fr.close();
+        fw.close();
+        reader.close();
     }
 }
